85e6bda50dfaf4cdf98fb5ef6d3f668360f12d33
k2lu <rigansust@gmail.com>
2021-12-04 20:55:11 +0600

    :)

---
+++ bfs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+std::vector <int> v[10];
+bool visited[10];
+int level[10];
+
+void bfs(int source)    {
+    std::queue <int> q;
+    q.push(source);
+    level[source] = 1;
+
+    while (!q.empty())  {
+        int w = q.front();
+        q.pop();
+
+        for (int i = 0; i < v[w].size(); i++)
+            if(visited[v[w][i]] == false)   {
+                q.push(v[w][i]);
+                visited[v[w][i]] = true;
+            }       
+    }    
+}
