6a76ec67706683d43a07f194e0915725dc375961
k2lu <rigansust@gmail.com>
2021-12-08 18:07:00 +0600

    :)

---
+++ graph/fenwicktree.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct FenwickTree  {
+    vector <int> bit;
+    int n;
+
+    FenwickTree(int n)  {
+        this -> n = n;
+        bit.assign(n, 0);
+    }
+
+    FenwickTree(vector <int> a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    int sum(int r)  {
+        int ret = 0;
+        for(; r>=0; r = (r&(r+1))-1)
+            ret += bit[r];
+        return ret;
+    }
+
+    int sum(int l, int r)   {
+        return sum(r) - sum(l-1);
+    }
+
+    void add(int idx, int delta)    {
+        for (0; idx < n; idx = idx|(idx+1))
+            bit[idx] += delta;
+    }
+};
